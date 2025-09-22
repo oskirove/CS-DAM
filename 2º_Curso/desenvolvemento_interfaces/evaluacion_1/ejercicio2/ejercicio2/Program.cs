fa562d171da62028c900d8e33abdbdce96c8223a
oscar <oskigames2004@gmail.com>
2025-09-22 09:47:39 +0200

    updates

---
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Introduce tu edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduce tu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0,-12}{1,4}\n\t{2,5:F1}\n\"{0}\" \\{1}\\", nombre, edad, peso);
+
+        }
+    }
+}
