71649aef9d9667e212009c9fd67745c4cb985aa0
oscar <oskigames2004@gmail.com>
2025-10-06 00:11:14 +0200

    update

--- 2º_Curso/desenvolvemento_interfaces/evaluacion_1/ejercicio5/ejercicio5/Program.cs
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/ejercicio5/ejercicio5/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ejercicio5
+﻿using System.CodeDom.Compiler;
+
+namespace ejercicio5
 {
     internal class Program
     {
+
+        public static bool calculoFactorial(ref int value)
+        {
+            if (value < 0) return false;
+
+            int acum = 1;
+
+            for (int i = value; i >= 1; i--)
+            {
+                acum *= i;
+            }
+
+            return true;
+        }
+
+        public static posicionesAleatorias()
+        {
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int num = 5;
+
+            Console.WriteLine(calculoFactorial(ref num));
+
+            Console.WriteLine("Hola mundo");
         }
     }
 }
