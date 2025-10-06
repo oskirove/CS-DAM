fa98b491f4134fec3299e5c2fc92f5ad482bae09
oscar <oskirove@gmail.com>
2025-10-06 18:29:09 +0200

    update

---
+++ 2º_Curso/acceso_a_datos/evaluacion_1/tema_2/ejercicios/ejercicio_dom/src/main/java/dev/Application.java
@@ -0,0 +1,16 @@
+
+package dev;
+
+import dev.lib.Functions;
+
+public class Application {
+  public static void main(String[] args) {
+    Functions f = new Functions();
+
+    //f.mostrarTitulosPeliculas();
+
+    // f.mostrarInfoPeliculas();
+
+    f.mostrarNDirectores(2);
+  }
+}
