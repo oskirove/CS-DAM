aea6b25e03c220e26649a036da204fbf32cf506e
oscar <oskigames2004@gmail.com>
2025-10-16 12:27:29 +0200

    update

---
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Ejercicio7/Ejercicio7/Clases/Astro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7.Clases
+{
+    internal abstract class Astro
+    {
+        protected String nombre;
+        protected double radio;
+
+        public Astro(string nombre, double radio)
+        {
+            this.nombre = nombre;
+            this.radio = radio;
+        }
+
+        public Astro()
+        {
+            nombre = "Tierra";
+            radio = 6378;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre.ToUpper();
+        }
+
+        public String getNombre()
+        {
+            return $"\"nombre\"";
+        }
+
+        public void setRadio(double radio)
+        {
+            if (radio < 0)
+            {
+                throw new ArgumentException("El radio no puede ser menor que cero");
+            }
+
+            this.radio = radio;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is String nombreString)
+            {
+                return nombre.Equals(nombreString, StringComparison.OrdinalIgnoreCase);
+            }
+
+            if (obj is Astro otroAstro)
+            {
+                return nombre.Equals(otroAstro.nombre, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Ejercicio7/Ejercicio7/Clases/Planeta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7.Clases
+{
+    internal class Planeta : Astro, ITerraformable
+    {
+        private bool gaseoso;
+        public int numSatelites;
+
+        public Planeta(String nombre, double radio, bool gaseoso, int numSatelites) : base(nombre, radio)
+        {
+            this.gaseoso = gaseoso;
+            this.numSatelites = numSatelites;
+        }
+
+        public Planeta() : this("", 1, false, 0)
+        {
+
+        }
+
+        public void setGaseoso(bool gaseoso)
+        {
+            this.gaseoso = gaseoso;
+        }
+
+        public bool getGaseoso()
+        {
+            return this.gaseoso;
+        }
+
+        public bool esHabitable()
+        {
+            return getGaseoso() == false && radio > 2000 && radio < 8000;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
