1f62401a7e9bc874a0be00a932b011607234be77
oscar <oskigames2004@gmail.com>
2025-10-23 13:00:33 +0200

    update

--- 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.Designer.cs
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(415, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Mouse Tester";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.cs
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.cs
@@ -15,6 +15,22 @@ namespace Ejercicio1
         public Form1()
         {
             InitializeComponent();
+
+            this.MouseLeave += Form_MouseLeave;
+            this.MouseMove += Form_MouseMove;
+            button1.MouseMove += Form_MouseMove;
+            button2.MouseMove += Form_MouseMove;
+        }
+
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = this.PointToClient(Cursor.Position);
+            this.Text = $"Mouse Tester - X:{p.X}, Y:{p.Y}";
+        }
+
+        private void Form_MouseLeave(object sender, EventArgs e)
+        {
+            this.Text = "Mouse Tester - X:0, Y:0";
         }
     }
 }
