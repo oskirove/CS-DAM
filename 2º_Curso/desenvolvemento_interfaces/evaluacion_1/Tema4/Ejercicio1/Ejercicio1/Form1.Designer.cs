08407bea7c2d0e33ffb29aca2aedd164d3088002
oscar <oskigames2004@gmail.com>
2025-10-24 10:45:09 +0200

    Update

--- 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.Designer.cs
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -50,15 +52,29 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(194, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(395, 106);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Pressed);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_NotPressed);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Mouse Tester";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +83,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.cs
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio1/Ejercicio1/Form1.cs
@@ -12,6 +12,7 @@ namespace Ejercicio1
 {
     public partial class Form1 : Form
     {
+        private string title = "Mouse Tester";
         public Form1()
         {
             InitializeComponent();
@@ -20,17 +21,39 @@ namespace Ejercicio1
             this.MouseMove += Form_MouseMove;
             button1.MouseMove += Form_MouseMove;
             button2.MouseMove += Form_MouseMove;
+            pictureBox1.MouseDown += pictureBox1_Pressed;
+            pictureBox1.MouseUp += pictureBox1_NotPressed;
+            this.KeyDown += Form_KeyPressed;
+
         }
 
         private void Form_MouseMove(object sender, MouseEventArgs e)
         {
             Point p = this.PointToClient(Cursor.Position);
-            this.Text = $"Mouse Tester - X:{p.X}, Y:{p.Y}";
+            this.Text = $"{title} - X:{p.X}, Y:{p.Y}";
         }
 
         private void Form_MouseLeave(object sender, EventArgs e)
         {
-            this.Text = "Mouse Tester - X:0, Y:0";
+            this.Text = $"{title} - X:0, Y:0";
         }
+
+        private void pictureBox1_Pressed(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Aqua;
+            button2.BackColor = Color.Red;
+        }
+        private void pictureBox1_NotPressed(object sender, EventArgs e)
+        {
+            button1.BackColor = SystemColors.Control;
+            button2.BackColor = SystemColors.Control;
+        }
+
+        private void Form_KeyPressed(object sender, KeyEventArgs e)
+        {
+            title = e.KeyCode.ToString();
+        }
+
+
     }
 }
