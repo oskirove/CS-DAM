54c9aeb22716c1c0f1cc107e3d16d5c019689012
oscar <oskirove@gmail.com>
2025-10-30 09:13:45 +0100

    Llegan los Ents a Java

--- 2º_Curso/afondamento/tema_git/Hola.java
+++ 2º_Curso/afondamento/tema_git/Hola.java
@@ -1,5 +1,9 @@
 class Hola {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
     System.out.println("Welcome to the Java World");
+    
+    System.out.println("I have no more branches to commit, said the Ent");
     }
+
 }
