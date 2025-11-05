67a5f54dfdfb2d264682c7efdbb73520717dae4f
oscar <oskigames2004@gmail.com>
2025-11-05 12:09:03 +0100

    update

---
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio4/Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(30, 155);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 229);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(310, 155);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(260, 229);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 69);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Añadir";
+            this.toolTip1.SetToolTip(this.button1, "Añadir Item");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 69);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Quitar";
+            this.toolTip1.SetToolTip(this.button2, "Eliminar Item");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(419, 431);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 69);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Traspasar";
+            this.toolTip1.SetToolTip(this.button3, "Mover Item");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(314, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Índices: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Contenido: ";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(600, 155);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(156, 29);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Acción en 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(600, 225);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(156, 29);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Acción en 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(600, 290);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(98, 29);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "1 -> 2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(600, 355);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(98, 29);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "2 -> 1";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(534, 31);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 525);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Añadir item";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ 2º_Curso/desenvolvemento_interfaces/evaluacion_1/Tema4/Ejercicio4/Ejercicio4/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        private string titulo = "Gestor de listas";
+        private Timer timer;
+        int cont = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 200;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            listBox1.SelectedIndexChanged += (s, e) => { label1.Text = "Índices: " + listBox1.SelectedIndex.ToString(); };
+        }
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            char[] letras = titulo.ToCharArray();
+
+            if (cont < letras.Length)
+            {
+                this.Text += letras[cont];
+                cont++;
+            }
+            else
+            {
+                cont = 0;
+                this.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                if (!listBox1.Items.Contains(textBox1.Text))
+                {
+                    string texto = textBox1.Text;
+                    listBox1.Items.Add(texto);
+                    label2.Text = "Contenido: " + listBox1.Items.Count.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo agregar a la lista, el contenido ya existe.", "Error al agregar el contenido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se pudo agregar a la lista, el campo no puede estar vacío.", "Error al agregar el contenido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                string seleccion = listBox1.SelectedItem?.ToString();
+                listBox1.Items.Remove(seleccion);
+                label2.Text = "Contenido: " + listBox1.Items.Count.ToString();
+            }
+
+            if (radioButton2.Checked)
+            {
+                string seleccion = listBox2.SelectedItem?.ToString();
+                listBox2.Items.Remove(seleccion);
+                label2.Text = "Contenido: " + listBox1.Items.Count.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                string seleccion = listBox1.SelectedItem?.ToString();
+
+                if (string.IsNullOrEmpty(seleccion))
+                {
+                    MessageBox.Show("debes seleccionar un item de la primera lista.");
+                }
+                else
+                {
+                    listBox2.Items.Add(seleccion);
+                    listBox1.Items.Remove(seleccion);
+                    label2.Text = "Contenido: " + listBox1.Items.Count.ToString();
+                }
+
+            }
+
+            if (radioButton4.Checked)
+            {
+                string seleccion = listBox2.SelectedItem?.ToString();
+
+                if (string.IsNullOrEmpty(seleccion))
+                {
+                    MessageBox.Show("debes seleccionar un item de la segunda lista.");
+                }
+                else
+                {
+                    listBox1.Items.Add(seleccion);
+                    listBox2.Items.Remove(seleccion);
+                    label2.Text = "Contenido: " + listBox1.Items.Count.ToString();
+                }
+            }
+        }
+
+        private void tooltip_Index(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
