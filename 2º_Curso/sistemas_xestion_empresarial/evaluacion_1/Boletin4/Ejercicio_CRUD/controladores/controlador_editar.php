28a3604bacef95b9446cb3bf8eeebfbbc7052590
oscar <oskigames2004@gmail.com>
2025-11-07 12:38:48 +0100

    update

--- 2º_Curso/sistemas_xestion_empresarial/evaluacion_1/Boletin4/Ejercicio_CRUD/controladores/controlador_editar.php
+++ 2º_Curso/sistemas_xestion_empresarial/evaluacion_1/Boletin4/Ejercicio_CRUD/controladores/controlador_editar.php
@@ -3,7 +3,7 @@ if (isset($_GET['uuid']) && is_numeric($_GET['uuid'])) {
     require_once '../modelos/modelo.php';
 
     $empleado = new Empleado();
-    $dato = $empleado->edit($_GET['uuid']);
+    $dato= $empleado->edit($_GET['uuid']);
 
     require_once '../vistas/vista_editar.php';
 }
