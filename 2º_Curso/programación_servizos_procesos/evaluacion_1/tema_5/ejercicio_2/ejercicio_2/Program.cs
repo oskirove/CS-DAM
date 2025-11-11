5c551b2f26eeda67aa23abb5f7176a4fbfd84aa8
oscar <oskigames2004@gmail.com>
2025-11-11 13:58:45 +0100

    update

---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_1/tema_5/ejercicio_2/ejercicio_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using ejercicio_2.lib;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("Carrera de caballos");
+                Console.WriteLine();
+                Console.WriteLine("1.- Iniciar carrera");
+                Console.WriteLine("2.- Salir del programa");
+                opcion = Utils.solicitarEntero("Introduce una opción: ");
+                Console.WriteLine();
+
+                switch (opcion)
+                {
+                    case 1:
+                        Utils utils = new Utils();
+
+                        int apuesta = Utils.solicitarEntero("Apuesta por un caballo [1 - 5]: ");
+                        Console.WriteLine();
+                        utils.inicializarCaballos(apuesta);
+
+                       
+                        break;
+                    case 2:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, elige 1 o 2.");
+                        break;
+                }
+            }
+            while (opcion != 2);
+        }
+    }
+}
---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_1/tema_5/ejercicio_2/ejercicio_2/lib/Utils.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2.lib
+{
+    internal class Utils
+    {
+        static bool running = true;
+        static readonly object key = new object();
+
+        public static int solicitarEntero(string mensaje)
+        {
+            while (true)
+            {
+                Console.Write(mensaje);
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int apuesta))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine();
+                    Console.WriteLine("ERROR: Lo que has introducido no es un número.");
+                    Console.WriteLine();
+                    Console.ResetColor();
+                    continue;
+                }
+
+                if (apuesta < 1 || apuesta > 5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine();
+                    Console.WriteLine("ERROR: Son 5 caballos, por lo que debes introducir un número entre 1 y 5 incluidos.");
+                    Console.WriteLine();
+                    Console.ResetColor();
+                    continue;
+                }
+
+                return apuesta;
+            }
+        }
+
+        public void inicializarCaballos(int apuesta)
+        {
+            Thread[] caballos = new Thread[5];
+
+            for (int i = 0; i < caballos.Length; i++)
+            {
+                int id = i;
+                caballos[i] = new Thread(correr);
+                caballos[i].Start(id);
+                Console.WriteLine();
+            }
+        }
+
+        public void correr(object obj)
+        {
+            int id = (int)obj;
+            StringBuilder pista = new StringBuilder();
+
+            while (true)
+            {
+                lock (key)
+                {
+                    if (!running) break;
+
+                    pista.Append(' ');
+                    Console.SetCursorPosition(0, id + 8);
+                    Console.Write(pista.ToString() + "*");
+                    Thread.Sleep(40);
+
+
+                    if (pista.Length >= 20)
+                    {
+                        running = false;
+                    }
+                }
+            }
+        }
+    }
+}
