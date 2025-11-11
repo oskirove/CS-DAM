ffcc4b8ebc71bc1a429a851d8eaa4ba29773d3c3
oscar <oskigames2004@gmail.com>
2025-11-11 10:47:30 +0100

    update

---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_1/tema_5/ejercicio_1/ejercicio_1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ejercicio_1
+{
+    internal class Program
+    {
+        static bool running = true;
+        static readonly object t = new object();
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+            Thread hilo1 = new Thread(() =>
+            {
+                while (running)
+                {
+                    lock (t)
+                    {
+                        contador++;
+
+                        Console.WriteLine("Hilo 1: " + contador);
+
+                        if (contador == 500)
+                        {
+                            running = false;
+                        }
+                    }
+                }
+            });
+
+            Thread hilo2 = new Thread(() =>
+            {
+                while (running)
+                {
+                    lock (t)
+                    {
+                        contador--;
+                        Console.WriteLine("Hilo 2: " + contador);
+                        if (contador == -500)
+                        {
+                            running = false;
+                        }
+                    }
+                }
+            });
+
+            hilo1.Start();
+            hilo2.Start();
+        }
+    }
+}
