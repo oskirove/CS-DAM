8d3adc27646313fb47ba5dbe4683c9c19fe7cb01
oscar <oskigames2004@gmail.com>
2025-11-23 21:00:02 +0100

    update

---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_1/ejercicios/ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notas = { 5, 2, 8, 1, 9, 4 };
+            string[] palabras = { "Sol", "Luna", "Estrella", "Cielo" };
+
+            if (notas.Any(nota => nota >= 5))
+            {
+                Console.WriteLine("Hay aprobados");
+            }
+            else
+            {
+                Console.WriteLine("No hay aprobados");
+            }
+
+            foreach (int nota in notas.Where(n => n >= 5))
+            {
+                Console.Write(nota + ", ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Índice del último aprobado: " + Array.FindLastIndex(notas, nota => nota >=5 ));
+
+            Console.WriteLine("El utlimo aprobado es: " + Array.FindLast(notas, nota => nota >= 5));
+
+            int[] notasPares = (Array.FindAll(notas, nota => nota % 2 == 0));
+
+            Console.WriteLine("La cantidad de notas pares es de: " + notasPares.Length);
+
+            Console.WriteLine();
+
+            // Segunda parte
+
+            Console.WriteLine(Array.Find(palabras, palabra => palabra.Length > 3));
+
+            Console.WriteLine();
+
+            Array.ForEach(palabras, palabra => Console.WriteLine(palabra.ToUpper()));
+
+            Console.WriteLine();
+            Console.WriteLine(Array.FindIndex(palabras, palabra => palabra.StartsWith("E")));
+        }
+    }
+}
