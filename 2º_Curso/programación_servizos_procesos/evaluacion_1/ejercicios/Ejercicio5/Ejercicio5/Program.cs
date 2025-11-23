0a1c06bca9d7ccfa30a6cbbe33e4080e956301f1
oscar <oskigames2004@gmail.com>
2025-11-24 00:05:00 +0100

    update

---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_1/ejercicios/Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,21 @@
+﻿using static Ejercicio5.Utils;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utils u = new Utils();
+
+            u.MenuGenerator(
+                new string[] { "Op1", "Op2", "Op3" },
+                new MyDelegate[] {
+                    () => Console.WriteLine("A"),
+                    () => Console.WriteLine("B"),
+                    () => Console.WriteLine("C")
+                }
+            );
+        }
+    }
+}
---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_1/ejercicios/Ejercicio5/Ejercicio5/Utils.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Utils
+    {
+        public delegate void MyDelegate();
+        public bool MenuGenerator(string[] opciones, MyDelegate[] delegados)
+        {
+
+            if (opciones == null || delegados == null)
+                return false;
+            if (opciones.Length != delegados.Length)
+                return false;
+            if (opciones.Length == 0)
+                return false;
+
+            int input = -1;
+
+            do
+            {
+                int cont = 0;
+
+                foreach (string opcion in opciones)
+                {
+                    cont++;
+                    Console.WriteLine($"{cont}.- {opcion}");
+                }
+                Console.WriteLine($"{opciones.Length + 1}.- Exit");
+
+
+                input = SolicitarEntero("Introduce una opcion: ");
+
+                if (input >= 1 && input <= opciones.Length)
+                {
+                    delegados[input - 1]();
+                }
+                else if (input == opciones.Length + 1)
+                {
+                    Console.WriteLine("Saliendo...");
+                }
+                else
+                {
+                    Console.WriteLine("Opción no válida.");
+                }
+            }
+            while (input != opciones.Length + 1);
+
+            return true;
+        }
+
+        private int SolicitarEntero(string mensaje)
+        {
+            while (true)
+            {
+                Console.WriteLine(mensaje);
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("El campo no puede estar vacío");
+                    continue;
+                }
+
+                bool result = int.TryParse(input, out int valor);
+
+                if (!result)
+                {
+                    Console.WriteLine("Debes introducir un número válido.");
+                    continue;
+                }
+
+                return valor;
+            }
+        }
+    }
+}
