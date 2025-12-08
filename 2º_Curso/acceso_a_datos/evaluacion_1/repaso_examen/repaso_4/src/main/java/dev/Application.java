86a1c6b9c105c41cd2f64055dcd3becf86479788
oscar <oskirove@gmail.com>
2025-12-08 16:56:31 +0100

    update

--- 2º_Curso/acceso_a_datos/evaluacion_1/repaso_examen/repaso_4/src/main/java/dev/Application.java
+++ 2º_Curso/acceso_a_datos/evaluacion_1/repaso_examen/repaso_4/src/main/java/dev/Application.java
@@ -9,6 +9,9 @@ public class Application {
   public static void main(String[] args) {
     Utils u = new Utils();
 
-    u.splitFileByChars(new File("repaso_4/src/main/java/dev/file.txt"), 10);
+    //u.splitFileByChars(new File("repaso_4/src/main/java/dev/file.txt"), 10);
+    u.splitFileByLines(new File("repaso_4/src/main/java/dev/file.txt"), 3);
+
+    
   }
 }
--- 2º_Curso/acceso_a_datos/evaluacion_1/repaso_examen/repaso_4/src/main/java/dev/lib/Utils.java
+++ 2º_Curso/acceso_a_datos/evaluacion_1/repaso_examen/repaso_4/src/main/java/dev/lib/Utils.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Utils {
     public void splitFileByChars(File file, int characters) {
@@ -24,6 +25,33 @@ public class Utils {
 
                     bw.write(new String(buffer, 0, caracteresLeidos));
                 }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void splitFileByLines(File file, int lines) {
+        if (!file.exists()) {
+            System.out.println("ERROR: El fichero al que intentas acceder no existe.");
+        }
+
+        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(file)))) {
+            int acum = 0;
+
+            while (sc.hasNextLine()) {
+                acum++;
+
+                try (BufferedWriter bw = new BufferedWriter(new FileWriter("fichero-" + acum))) {
+
+                    for (int i = 0; i < lines && sc.hasNextLine(); i++) {
+                        bw.write(sc.nextLine() + "\n");
+                    }
+                }
+            }
+
+            for (int i = 0; i < lines; i++) {
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
