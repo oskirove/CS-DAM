39e904f1313083a5facab4d31138ea1a2f4d3757
oscar <oskigames2004@gmail.com>
2026-01-08 12:34:01 +0100

    update

---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/Inicio_networking/Inicio_networking/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Net.Sockets;
+
+namespace Inicio_networking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+}
