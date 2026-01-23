7c7f5f9b52c20ef8c4d389a12c00a7582cc6518a
oscar <oskigames2004@gmail.com>
2026-01-23 13:49:29 +0100

    update

---
+++ 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/ejercicio_3/servidor/ShiftServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace servidor
+{
+    internal class ShiftServer
+    {
+        private string[] Users { get; set; }
+        private List<string> WaitQueue { get; set; }
+
+        private void Init()
+        {
+
+        }
+
+        private void ReadNames(FileInfo filePath)
+        {
+
+            List<string> allUsers = new List<string>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath.FullName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        allUsers.AddRange(line.Split(";"));
+                    }
+
+                    Users = allUsers.ToArray();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"ERROR: {e.Message}");
+            }
+        }
+
+        private int ReadPin(FileInfo filePath)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath.FullName))
+                {
+                    string line = sr.ReadLine();
+                    int[] numeros = new int[4];
+                }
+            }
+            catch (IOException e)
+            {
+                return -1;
+            }
+        }
+    }
+}
