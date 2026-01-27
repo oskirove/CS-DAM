aa58f470fc18d0af756f0a30f1ac437fa2068804
oscar <oskigames2004@gmail.com>
2026-01-27 12:24:29 +0100

    update

---
+++ 2º_Curso/sistemas_xestion_empresarial/evaluacion_2/libreria/database/migrations/2026_01_27_105841_create_libros_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->engine('InnoDB');
+            $table->id();
+            $table->bigInteger('categoria_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libros');
+    }
+};
