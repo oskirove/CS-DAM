72a2b41b931bd9c232ae771089ae43865e829962
oscar <oskigames2004@gmail.com>
2026-02-02 10:03:34 +0100

    update

--- 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/ejercicio_4/servidor/GameServer.cs
+++ 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/ejercicio_4/servidor/GameServer.cs
@@ -11,6 +11,7 @@ namespace servidor
         private Socket MainSocket;
         int Port { get; set; } = 4321;
         private bool ServerRunning { get; set; } = true;
+        private readonly string WordsPath = $"{Environment.GetEnvironmentVariable("USERPROFILE")}/words.txt";
 
         public void InitServer()
         {
@@ -23,17 +24,71 @@ namespace servidor
                 {
                     try
                     {
-
+                        ie = new IPEndPoint(IPAddress.Any, Port);
+                        MainSocket.Bind(ie);
                     }
-                    catch ()
+                    catch (SocketException e)
                     {
+                        if (e.ErrorCode == 10048)
+                        {
+                            Console.WriteLine($"Error: Puerto {Port} ocupado, probando con el siguiente.");
+                            Port++;
+                        }
+                    }
+                }
 
+                MainSocket.Listen(10);
+                Console.WriteLine($"Servidor en escucha {ie.Address}:{Port}");
+                Console.WriteLine($"Esperando conexiones... (Ctrl+c para salir)");
+
+                while (ServerRunning)
+                {
+                    try
+                    {
+                        Socket client = MainSocket.Accept();
+                        Thread hilo = new Thread(() => ClientDispatcher(client));
+
+                        hilo.Start();
+                    }
+                    catch (SocketException e)
+                    {
+                        ServerRunning = false;
                     }
                 }
             }
         }
 
         private void ClientDispatcher(Socket sClient)
+        {
+            try
+            {
+                using (sClient)
+                {
+                    IPEndPoint ieClient = (IPEndPoint)sClient.RemoteEndPoint;
+                    Console.WriteLine($"Cliente conectado: {ieClient.Address} en el puerto {ieClient.Port}");
+                    Encoding encoding = Console.OutputEncoding;
+
+                    using (NetworkStream ns = new NetworkStream(sClient))
+                    using (StreamReader sr = new StreamReader(ns, encoding))
+                    using (StreamWriter sw = new StreamWriter(ns, encoding))
+                    {
+                        sw.AutoFlush = true;
+
+                        sw.WriteLine("--------- JUEGO DEL AHORCADO ---------");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"El cliente cerró la conexión: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error inesperado: {e.Message}");
+            }
+        }
+
+        private void ReadWordsFile(string path)
         {
 
         }
