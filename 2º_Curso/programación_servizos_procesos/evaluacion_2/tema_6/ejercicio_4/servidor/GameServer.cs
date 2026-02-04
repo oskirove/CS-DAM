5ea0f283b9dda5e7bafe3f8192a02ff2681e30be
oscar <oskigames2004@gmail.com>
2026-02-04 11:46:55 +0100

    udpate

--- 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/ejercicio_4/servidor/GameServer.cs
+++ 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/ejercicio_4/servidor/GameServer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Xml.Linq;
 
 namespace servidor
 {
@@ -12,8 +13,10 @@ namespace servidor
         int Port { get; set; } = 4321;
         private bool ServerRunning { get; set; } = true;
         private readonly object key = new object();
-        private readonly string WordsPath = $"{Environment.GetEnvironmentVariable("USERPROFILE")}/words.txt";
+        private readonly FileInfo WordsFile = new FileInfo($"{Environment.GetEnvironmentVariable("USERPROFILE")}/words.txt");
+        private readonly FileInfo RecordsFile = new FileInfo($"{Environment.GetEnvironmentVariable("USERPROFILE")}/records.bin");
         private List<string> Words;
+        private List<Record> Records;
 
         public void InitServer()
         {
@@ -68,13 +71,21 @@ namespace servidor
                 using (sClient)
                 {
                     bool stopClient = false;
-                    string message;
+                    string username = null;
+                    string command;
+                    int time = 0;
+                    System.Timers.Timer timer = new System.Timers.Timer(1000);
+
+                    timer.Elapsed += (s, e) =>
+                    {
+                        time++;
+                    };
 
                     IPEndPoint ieClient = (IPEndPoint)sClient.RemoteEndPoint;
                     Console.WriteLine($"Cliente conectado: {ieClient.Address} en el puerto {ieClient.Port}");
                     Encoding encoding = Console.OutputEncoding;
 
-                    Words = ReadWordsFile(WordsPath);
+                    Words = ReadWordsFile();
 
                     using (NetworkStream ns = new NetworkStream(sClient))
                     using (StreamReader sr = new StreamReader(ns, encoding))
@@ -84,17 +95,74 @@ namespace servidor
 
                         sw.WriteLine("--------- JUEGO DEL AHORCADO ---------");
 
-                        while (!stopClient && (message = sr.ReadLine()) != null)
+                        sw.Write("Introduce tu nombre de usuario: ");
+                        username = sr.ReadLine();
+                        timer.Start();
+
+                        while (!stopClient && (command = sr.ReadLine()) != null)
                         {
-                            switch (message)
+                            string[] parts = command.Split(" ", 2);
+                            string cmd = parts[0];
+                            string arg = parts.Length > 1 ? parts[1] : null;
+
+                            switch (cmd)
                             {
                                 case "gw":
+
                                     Random rand = new Random();
                                     int num = (int)rand.Next(0, Words.Count);
+
+                                    sw.WriteLine(Words[num]);
+
                                     break;
                                 case "sw":
 
+                                    if (!string.IsNullOrEmpty(arg))
+                                    {
+                                        if (Words.Contains(arg))
+                                        {
+                                            sw.WriteLine("Esta palabra ya existe en el archivo.");
+                                        }
+                                        else
+                                        {
+                                            lock (key)
+                                            {
+                                                Words.Add(arg);
+                                                saveWord(arg);
+                                            }
+
+                                            sw.WriteLine($"Se ha añadido {arg} al archivo.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        sw.WriteLine("Comando incorrecto.");
+                                        sw.WriteLine("Uso: sw <palabra>");
+                                    }
+
                                     break;
+                                case "gr":
+                                    Records = getRecords();
+                                    foreach (Record r in Records)
+                                    {
+                                        sw.WriteLine($"{r.User} -> {r.Seconds}");
+                                    }
+                                    break;
+                                case "sr":
+
+                                    Record record = new Record(username, time);
+
+                                    lock (key)
+                                    {
+                                        saveRecord(record);
+                                        Records.Add(record);
+                                    }
+
+                                    break;
+                                case "close":
+
+                                    break;
+
                             }
                         }
                     }
@@ -110,13 +178,13 @@ namespace servidor
             }
         }
 
-        private List<string> ReadWordsFile(string path)
+        private List<string> ReadWordsFile()
         {
-            List<String> words = new List<String>();
+            List<string> words = new List<string>();
 
             try
             {
-                using (StreamReader sr = new StreamReader(path))
+                using (StreamReader sr = new StreamReader(WordsFile.FullName))
                 {
                     string line;
                     while ((line = sr.ReadLine()) != null)
@@ -130,6 +198,62 @@ namespace servidor
             catch (IOException e)
             {
                 return null;
+            }
+        }
+
+        private List<Record> getRecords()
+        {
+            List<Record> records = new List<Record>();
+            try
+            {
+                using (FileStream fs = new FileStream(RecordsFile.FullName, FileMode.Open, FileAccess.Read))
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    while (fs.Position < fs.Length)
+                    {
+                        string user = br.ReadString();
+                        int time = br.ReadInt32();
+
+                        records.Add(new Record(user, time));
+                    }
+                }
+                return records;
+            }
+            catch (IOException e)
+            {
+                return null;
+            }
+        }
+
+        private void saveWord(string word)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(WordsFile.FullName, true))
+                {
+                    sw.Write($",{word}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: no se pudo abrir el archivo");
+            }
+        }
+
+        private void saveRecord(Record record)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(RecordsFile.FullName, FileMode.Append))
+                using (BinaryWriter bw = new BinaryWriter(stream))
+                {
+                    bw.Write(record.User);
+                    bw.Write(record.Seconds);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: no se pudo abrir el archivo");
             }
         }
     }
--- 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/ejercicio_4/servidor/Record.cs
+++ 2º_Curso/programación_servizos_procesos/evaluacion_2/tema_6/ejercicio_4/servidor/Record.cs
@@ -7,6 +7,13 @@ namespace servidor
     internal class Record
     {
         public string User { get; set; }
-        public int seconds { get; set; }
+        public int Seconds { get; set; }
+
+
+        public Record(string user, int seconds)
+        {
+            User = user;
+            Seconds = seconds;
+        }
     }
 }
